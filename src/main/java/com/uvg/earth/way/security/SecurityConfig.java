ae276c8c9e1113bda58faa72db59f0ceb775978c
carlosaltan18 <carlosaltan850@gmail.com>
2025-10-17 15:00:19 -0600

    Fix spring security

--- src/main/java/com/uvg/earth/way/security/SecurityConfig.java
+++ src/main/java/com/uvg/earth/way/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/auth/**").permitAll()
-                                .requestMatchers("/swagger-ui.html").permitAll()
+                                .requestMatchers("/", "/swagger-ui.html").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/swagger-resources/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
